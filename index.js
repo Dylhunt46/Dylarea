d0d61ffac2403560d67e888644b53ebf58930e71
Laurent Houssin <dylhunt46@gmail.com>
2023-12-27 13:56:30 +0100

    Create Carousel

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+// Carousel
+const carouselButtons = document.querySelectorAll('.carousel-btn');
+const slides = document.querySelectorAll('.slide');
+
+carouselButtons.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    const calcNextSlide = e.target.id === 'next' ? 1 : -1;
+    const slideActive = document.querySelector('.active');
+
+    newIndex = calcNextSlide + [...slides].indexOf(slideActive);
+    if (newIndex < 0) newIndex = [...slides].length - 1;
+    if (newIndex > [...slides].length - 1) newIndex = 0;
+    slides[newIndex].classList.add('active');
+    slideActive.classList.remove('active');
+  });
+});
+
+//Bonjour
 setTimeout(function () {
   customElements.define(
     'count-up',
